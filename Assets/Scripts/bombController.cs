bb56b8df83ab92148794669234dc0ac7a4564377
Victor Nguyen <victorng1902@gmail.com>
2015-11-28 17:53:51 -0600

    finish bomb mechanics

--- Assets/Scripts/bombController.cs
+++ Assets/Scripts/bombController.cs
@@ -4,14 +4,12 @@ using System.Collections.Generic;
 
 public class bombController : MonoBehaviour {
 
-	float timer = 2.0f;
-	RaycastHit2D[]  linecast;
-
-	int radius = 5;
-	bool called = false;
+	public float timer = 2.0f;
+	List<RaycastHit2D> toBeDestroyed; // list of temp objects to be destroyed later when line-casting against other colliders on bomb path
+	int radius = 6; // bomb radius
 	// Use this for initialization
 	void Start () {
-	
+		toBeDestroyed = new List<RaycastHit2D>();
 	}
 	
 	// Update is called once per frame
@@ -20,13 +18,15 @@ public class bombController : MonoBehaviour {
 		//Debug.Log (Time.deltaTime +" - "+timer);
 		if (timer < 0.0f) {
 			Debug.Log ("Boom");
-			gameObject.SetActive(false);
-			if (!called){
-				spawnExplosionSprite();
-				called = true;
-			}
-			Destroy(gameObject);
+			customDestroyBomb();
 		}
+	}
+
+	public void customDestroyBomb(){
+		Debug.Log ("About to destroy: "+ gameObject.name);
+		gameObject.SetActive(false);
+		spawnExplosionSprite();
+		Destroy(gameObject);
 	}
 
 	void spawnExplosionSprite(){
@@ -35,91 +35,75 @@ public class bombController : MonoBehaviour {
 
 		GameObject expCenter = Instantiate (Resources.Load ("Prefabs/explosion")) as GameObject;
 		expCenter.transform.position = bombpos;
-		SpawnExplosion (bombCenter, new Vector3 (radius*(-1.0f)-0.4f,0.0f,0.0f), "left", bombpos);
-		SpawnExplosion (bombCenter, new Vector3 (radius*(1.0f)+0.4f,0.0f,0.0f), "right", bombpos);
-		SpawnExplosion (bombCenter, new Vector3 (0.0f, radius*(1.0f)+0.4f,0.0f), "up", bombpos);
-		SpawnExplosion (bombCenter, new Vector3 (0.0f, radius*(-1.0f)-0.4f,0.0f), "down", bombpos);
+		SpawnExplosionInGivenDirection (bombCenter, new Vector3 (radius*(-1.0f)-0.4f,0.0f,0.0f), "left", bombpos);
+		SpawnExplosionInGivenDirection (bombCenter, new Vector3 (radius*(1.0f)+0.4f,0.0f,0.0f), "right", bombpos);
+		SpawnExplosionInGivenDirection (bombCenter, new Vector3 (0.0f, radius*(1.0f)+0.4f,0.0f), "up", bombpos);
+		SpawnExplosionInGivenDirection (bombCenter, new Vector3 (0.0f, radius*(-1.0f)-0.4f,0.0f), "down", bombpos);
 
 	}
 
-	void SpawnExplosion(Vector3 bombCenter, Vector3 end, string direction, Vector3 bombPos){
+	void SpawnExplosionInGivenDirection(Vector3 bombCenter, Vector3 end, string direction, Vector3 bombPos){
 		Vector3 spawnPos = Vector3.zero;
-		Vector3 tmp = offsetVector(direction);
-
+		Vector3 unitVector = findUnitVector(direction);
+		toBeDestroyed.Clear ();
 		// line cast all return all objects hit by the line from startPoint to endPoint
 		RaycastHit2D[] detectedObjects = Physics2D.LinecastAll(bombCenter, bombCenter + end);
 
 		if (detectedObjects.Length == 0) {
-			//maxout bomb radius
+			//No obstacles on the path - simply spawn the exp sprites
 			for (int i = 0, j=1; i < radius; i++){
 				GameObject explosionTempSprite = Instantiate (Resources.Load ("Prefabs/explosion")) as GameObject;
-				explosionTempSprite.transform.position = bombPos + new Vector3 (j * tmp.x, j * tmp.y, 0.0f);
+				explosionTempSprite.transform.position = bombPos + new Vector3 (j * unitVector.x, j * unitVector.y, 0.0f);
 				j++;
 			}
-		} else { // collide at least one
-			string tag = "";
+		} else { // collide at least one obstacles
+			string obstacleTag = "";
 			bool notDone = true;
 			GameObject firstTerrainHit = detectedObjects[0].collider.gameObject; // will be updated
-			int firstTerrainHitIndex = 0;
-			//1. we look for the first terrain hit
+			int bombCountOnPath = 0; // use this to make sure that only the last bomb on the path gets to destroy destructible terrain
+
+			//1. we look for the first terrain hit - this is the boundary to spawn exp sprites
 			for (int i = 0; i < detectedObjects.Length && notDone;i++){
-				tag = detectedObjects[i].collider.gameObject.tag;
-				if (tag == "nonDestructible" || tag == "destructible"){
+				obstacleTag = detectedObjects[i].collider.gameObject.tag;
+				if (obstacleTag == "nonDestructible" || obstacleTag == "destructible"){
 					// if we find it - then we save its meta data to process later
 					spawnPos = calculateSpawnPosition(detectedObjects[i].collider.transform.position, direction);
 					firstTerrainHit = detectedObjects[i].collider.gameObject;
-					firstTerrainHitIndex = i;
 					notDone = false;
 				}
+				else { // items, monsters and character - possibly another bomb included - note that exp sprites dont have collider so they wont get caught here
+					if (obstacleTag == "bomb") 
+						bombCountOnPath++;	
+					toBeDestroyed.Add(detectedObjects[i]);
+				}
 			} 
-			//
-			// note that monster might be hit before terrain
-			//
-			//2. now we spawn exp sprites - and destroy monster/character if theres any between bom pos and first hit terrain
-			if (tag == "nonDestructible"){
-				int count = (int) Vector2.Distance(spawnPos,bombPos);
-				for (int i = 0, j=1; i < count; i++){
-					GameObject explosionTempSprite = Instantiate (Resources.Load ("Prefabs/explosion")) as GameObject;
-					explosionTempSprite.transform.position = bombPos + new Vector3 (j * tmp.x, j * tmp.y, 0.0f);
-					j++;
-				}
-				// also we destroy any monster or character in between
-				for (int i = 0; i < firstTerrainHitIndex;i++){
-					// we need to call customDestroy 
-					detectedObjects[i].collider.gameObject.GetComponent<Destroy>().customDestroy();
-				}
+
+			//2. now we spawn exp sprites 
+			int count = 0;
+			// find how many sprites we need to spawn
+			if (obstacleTag == "nonDestructible" || obstacleTag == "destructible")
+				count = (int) Vector2.Distance(spawnPos,bombPos);
+			else  // "" by default - meaning all objects hit are either monster or character or bomb --> spawn all sprites equal to radius value
+				count = radius;
+			// spawn exp sprites
+			for (int i = 0, j=1; i < count; i++){
+				GameObject explosionTempSprite = Instantiate (Resources.Load ("Prefabs/explosion")) as GameObject;
+				explosionTempSprite.transform.position = bombPos + new Vector3 (j * unitVector.x, j * unitVector.y, 0.0f);
+				j++;
 			}
-			else if (tag == "destructible") {
-				int count = (int) Vector2.Distance(spawnPos,bombPos);
-				for (int i = 0, j=1; i < count; i++){
-					GameObject explosionTempSprite = Instantiate (Resources.Load ("Prefabs/explosion")) as GameObject;
-					explosionTempSprite.transform.position = bombPos + new Vector3 (j * tmp.x, j * tmp.y, 0.0f);
-					j++;
-				}
+			// also we spawn exp sprite for destructible terrain block - only if this bomb is the last or only on the path in given direction
+			if (obstacleTag == "destructible" && bombCountOnPath == 0){
 				// spawn explosion sprite for wood block
 				firstTerrainHit.SetActive(false); // hide it first
-				GameObject tmp1 = Instantiate(Resources.Load("Prefabs/blockExplosion") as GameObject);
+				GameObject tmp1 = Instantiate(Resources.Load("Prefabs/blockExplosion")) as GameObject;
 				tmp1.transform.position = firstTerrainHit.transform.position;			
 				Destroy (firstTerrainHit);
-
-				// also we destroy any monster or character in between
-				for (int i = 0; i < firstTerrainHitIndex;i++){
-					// we need to call customDestroy 
-					detectedObjects[i].collider.gameObject.GetComponent<Destroy>().customDestroy();
-				}
-
 			}
-			else { // "" by default - meaning all objects hit are either monster or character
-				for (int i = 0, j=1; i < radius; i++){
-					GameObject explosionTempSprite = Instantiate (Resources.Load ("Prefabs/explosion")) as GameObject;
-					explosionTempSprite.transform.position = bombPos + new Vector3 (j * tmp.x, j * tmp.y, 0.0f);
-					j++;
-				}
-				// Then we destroy any monster or character in between
-				for (int i = 0; i < detectedObjects.Length;i++){
-					// we need to call customDestroy 
-					detectedObjects[i].collider.gameObject.GetComponent<Destroy>().customDestroy();
-				}
+
+			// 3. Now we destroy any monster or character in between - or bomb
+			for (int i = 0; i < toBeDestroyed.Count; i++){
+				// we need to call customDestroy 
+				destroyBasedOnTag(toBeDestroyed[i].collider.gameObject);
 			}
 		}
 	} // end
@@ -127,6 +111,15 @@ public class bombController : MonoBehaviour {
 
 
 	// utilities
+	void destroyBasedOnTag(GameObject theObject){
+		// we need to call customDestroy 
+		if (theObject.tag == "bomb")
+			theObject.GetComponent<bombController>().customDestroyBomb();
+		else if (theObject.tag == "character")
+			theObject.GetComponent<xoController>().customDestroyCharacter();
+		else // items and monsters
+			theObject.GetComponent<Destroy>().customDestroy();
+	}
 	Vector3 calculateSpawnPosition(Vector3 givenPoint, string direction){
 		Vector3 result = Vector3.zero;
 		if (direction == "left"){
@@ -140,7 +133,7 @@ public class bombController : MonoBehaviour {
 		}
 		return result;
 	} // end
-	Vector3 offsetVector(string direction){
+	Vector3 findUnitVector(string direction){
 		Vector3 result = Vector3.zero;
 		if (direction == "left"){
 			result = new Vector3(-1.0f,0.0f,0.0f);
--- Assets/Scripts/xoController.cs
+++ Assets/Scripts/xoController.cs
@@ -5,6 +5,7 @@ public class xoController : MonoBehaviour {
 	
 	public Animator anim;
 	public Rigidbody2D rbody;
+	SpriteRenderer spriteRenderer;
 	Vector2 directionBeforeBlocked;
 	Vector2 targetPoint;
 	
@@ -12,6 +13,7 @@ public class xoController : MonoBehaviour {
 	void Start () {
 		rbody = GetComponent<Rigidbody2D> ();
 		anim = GetComponent<Animator> ();
+		spriteRenderer = GetComponent<SpriteRenderer>();
 		anim.SetBool("isWalking", false);
 		anim.SetFloat ("input_x", 0.0f);
 		anim.SetFloat ("input_y", -1.0f);
@@ -19,6 +21,10 @@ public class xoController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (!anim.enabled) {
+			rbody.MovePosition(rbody.position);
+			return;
+		}
 		GetComponent<SpriteRenderer>().sortingOrder = Mathf.RoundToInt(transform.position.y)*(-1);
 		Vector2 movement = new Vector2 (Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
 		if (movement != Vector2.zero) {
@@ -33,9 +39,21 @@ public class xoController : MonoBehaviour {
 		rbody.MovePosition (targetPoint);
 		
 	}
-	void OnCollisionEnter2D(Collision2D col)
-	{	
-
+	public void customDestroyCharacter(){
+		StartCoroutine (timer());
+	}
+	IEnumerator timer(){
+		anim.enabled = false;
+		spriteRenderer.color = new Color (1.0f,1.0f,1.0f,1.0f); 
+		yield return new WaitForSeconds(0.2f);
+		spriteRenderer.color = new Color (1.0f,1.0f,1.0f,0.75f); 
+		yield return new WaitForSeconds(0.2f);
+		spriteRenderer.color = new Color (1.0f,1.0f,1.0f,0.5f); 
+		yield return new WaitForSeconds(0.2f);
+		spriteRenderer.color = new Color (1.0f,1.0f,1.0f,0.25f); 
+		yield return new WaitForSeconds(0.2f);
+		spriteRenderer.color = new Color (1.0f,1.0f,1.0f,0.0f); 
+		Destroy (gameObject);
 	}
 
 
